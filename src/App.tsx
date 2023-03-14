ae090f83f8abe8462de4550819e16ef993e3b821
Samuel Jones <Samuel.Jones@victorianplumbing.co.uk>
2023-03-14 14:07:00 +0000

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, SyntheticEvent } from "react";
+import axios from "axios";
 
-function App() {
+export function App() {
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    setIsLoading(true)
+
+    // Send a request to the server with the prompt
+    axios
+      .post(`${process.env.REACT_APP_API_BASE_URL}/chat`, { prompt })
+      .then((res) => {
+        // Update the response state with the server's response
+        setResponse(res.data);
+        setIsLoading(false)
+      })
+      .catch((err) => {
+        console.error(err);
+        setIsLoading(false)
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{isLoading ? 'Generating ideas...' : response}</p>
     </div>
   );
 }
-
-export default App;
