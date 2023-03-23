ba1aaf545dd264d99c8b9ff7a8b749d73bab01ba
Samuel Jones <Samuel.Jones@victorianplumbing.co.uk>
2023-03-23 14:34:29 +0000

    Add linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Card from './Card'
-import { Skeleton, Typography, Input, InputLabel, Fab, TextField  } from '@mui/material'
-import { Wrapper, Header, Main, Footer, FooterText, ActionsWrapper } from "./App.styles";
-import { AcUnit, Lightbulb } from "@mui/icons-material";
+import { Skeleton, Typography, Fab, TextField } from '@mui/material'
+import {
+  Wrapper,
+  Header,
+  Main,
+  Footer,
+  FooterText,
+  ActionsWrapper,
+} from './App.styles'
+import { AcUnit, Lightbulb } from '@mui/icons-material'
 
 const DEFAULT_THEME = 'environmental'
 
-type ChatResponse = {
-  title: string; 
-  tagline: string; 
+interface ChatResponse {
+  title: string
+  tagline: string
   description: string
   keywords: string[]
 }
 
-export function App() {
-  const [appTheme, setAppTheme] = useState(DEFAULT_THEME);
-  const [response, setResponse] = useState<ChatResponse | null>(null);
+export function App(): JSX.Element {
+  const [appTheme, setAppTheme] = useState(DEFAULT_THEME)
+  const [response, setResponse] = useState<ChatResponse | null>(null)
   const [isLoading, setIsLoading] = useState(false)
   const [errorMessage, setErrorMessage] = useState('')
 
-  const fetchIdeas = () => {
+  const fetchIdeas = (): void => {
     setIsLoading(true)
     setErrorMessage('')
-    
+
     const prompt = `Give me a new app idea in the following JSON format: 
     title, tagline (without referring to the title), description (maximum 400 characters), keywords (maximum 5). 
     The app theme should be ${appTheme}.`
@@ -31,19 +38,23 @@ export function App() {
     const controller = new AbortController()
 
     axios
-      .post(`${process.env.REACT_APP_API_BASE_URL}/chat`, { prompt }, { signal: controller.signal, timeout: 10000 })
+      .post(
+        `${process.env.REACT_APP_API_BASE_URL as string}/chat`,
+        { prompt },
+        { signal: controller.signal, timeout: 10000 }
+      )
       .then((res) => {
-        setResponse(res.data);
+        setResponse(res.data)
         setIsLoading(false)
       })
-      .catch((err) => {
-        setErrorMessage(err?.response?.data?.message || 'Oops! Something went wrong')
-        setResponse(null);
+      .catch(() => {
+        setErrorMessage('Oops! Something went wrong')
+        setResponse(null)
         setIsLoading(false)
-      });
-  };
+      })
+  }
 
-  const handleThemeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleThemeChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     if (e.target.value === '') {
       setAppTheme(DEFAULT_THEME)
     } else {
@@ -55,57 +66,74 @@ export function App() {
     fetchIdeas()
   }, [])
 
-  const formattedKeywords = response?.keywords?.map(keyword => keyword.replace(/ /g, ''))
+  const formattedKeywords = response?.keywords?.map((keyword) =>
+    keyword.replace(/ /g, '')
+  )
 
   return (
     <Wrapper>
       <Header>
-        <Typography gutterBottom variant="h1" color="whitesmoke">AI App Ideas</Typography>
-        <Typography variant="h5" color="whitesmoke">Find your next app idea with the help of AI</Typography>
+        <Typography gutterBottom variant="h1" color="whitesmoke">
+          AI App Ideas
+        </Typography>
+        <Typography variant="h5" color="whitesmoke">
+          Find your next app idea with the help of AI
+        </Typography>
       </Header>
 
       <Main>
-      {!isLoading && errorMessage &&
-      <>
-        <AcUnit color="warning" />
-        <Typography color='whitesmoke' variant='h6'>{errorMessage}</Typography>
-        </>
-      }
-      {isLoading &&
-        <>
-          <Skeleton variant="rectangular" width={350} height={300} />
-          <Skeleton variant="rounded" width={350} height={50} />
-          <Skeleton variant="rounded" width={350} height={50} />
-        </>
-      } 
+        {!isLoading && errorMessage.length > 0 && (
+          <>
+            <AcUnit color="warning" />
+            <Typography color="whitesmoke" variant="h6">
+              {errorMessage}
+            </Typography>
+          </>
+        )}
+        {isLoading && (
+          <>
+            <Skeleton variant="rectangular" width={350} height={300} />
+            <Skeleton variant="rounded" width={350} height={50} />
+            <Skeleton variant="rounded" width={350} height={50} />
+          </>
+        )}
 
-      {!isLoading && 
-        response &&
-          <Card 
-            title={response.title} 
+        {!isLoading && response != null && (
+          <Card
+            title={response.title}
             tagline={response.tagline}
-            description={response.description} 
-            image={`https://source.unsplash.com/random/?${formattedKeywords?.[0] || response.title}`} 
-            keywords={formattedKeywords || []} 
+            description={response.description}
+            image={`https://source.unsplash.com/random/?${
+              formattedKeywords != null ? formattedKeywords[0] : response.title
+            }`}
+            keywords={formattedKeywords != null ? formattedKeywords : []}
           />
-      }
-      <ActionsWrapper>
-      <TextField id="change-app-theme" label="Change theme" 
-        variant="standard" 
-        color='primary' 
-        disabled={isLoading} 
-        value={appTheme === DEFAULT_THEME ? '' : appTheme}
-        onChange={handleThemeChange}
-        />
-      <Fab color="primary" variant="extended" sx={{ borderRadius: 1 }} onClick={fetchIdeas} disabled={isLoading}>
+        )}
+        <ActionsWrapper>
+          <TextField
+            id="change-app-theme"
+            label="Change theme"
+            variant="standard"
+            color="primary"
+            disabled={isLoading}
+            value={appTheme === DEFAULT_THEME ? '' : appTheme}
+            onChange={handleThemeChange}
+          />
+          <Fab
+            color="primary"
+            variant="extended"
+            sx={{ borderRadius: 1 }}
+            onClick={fetchIdeas}
+            disabled={isLoading}
+          >
             <Lightbulb sx={{ mr: 1 }} />
             Show me a new idea
-      </Fab>
-      </ActionsWrapper>
+          </Fab>
+        </ActionsWrapper>
       </Main>
       <Footer>
         <FooterText>Powered by ChatGPT</FooterText>
       </Footer>
     </Wrapper>
-  );
+  )
 }
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,35 +1,43 @@
-import { Card, Button, CardActions, Fade, CardContent, Typography, CardMedia } from '@mui/material';
-import { Share } from '@mui/icons-material';
+import {
+  Card,
+  Button,
+  CardActions,
+  Fade,
+  CardContent,
+  Typography,
+  CardMedia,
+} from '@mui/material'
+import { Share } from '@mui/icons-material'
 import styled from '@emotion/styled'
 
 const KeywordWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    width: 100%;
-    gap: 4px;
-    margin-top: 16px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  gap: 4px;
+  margin-top: 16px;
 `
 
-type CardProps = {
-    title: string; 
-    tagline: string; 
-    description: string; 
-    image: string; 
-    keywords: string[]
+interface CardProps {
+  title: string
+  tagline: string
+  description: string
+  image: string
+  keywords: string[]
 }
 
-
-export default function IdeaCard({ title, tagline, description, image, keywords }: CardProps) {
+export default function IdeaCard({
+  title,
+  tagline,
+  description,
+  image,
+  keywords,
+}: CardProps): JSX.Element {
   return (
     <Fade in={Boolean(title)}>
-    <Card sx={{ maxWidth: 400, maxHeight: 600 }}>
-        <CardMedia
-          component="img"
-          height="160"
-          alt={title}
-          src={image}
-        />
+      <Card sx={{ maxWidth: 400, maxHeight: 600 }}>
+        <CardMedia component="img" height="160" alt={title} src={image} />
         <CardContent>
           <Typography gutterBottom variant="h5">
             {title}
@@ -40,18 +48,25 @@ export default function IdeaCard({ title, tagline, description, image, keywords 
           <Typography gutterBottom variant="body2" color="text.secondary">
             {description}
           </Typography>
-            <KeywordWrapper>
-          {keywords.map(keyword => (
-                <Typography key={keyword} variant="body2" color="text.secondary" fontWeight={500}>#{keyword}</Typography>
+          <KeywordWrapper>
+            {keywords.map((keyword) => (
+              <Typography
+                key={keyword}
+                variant="body2"
+                color="text.secondary"
+                fontWeight={500}
+              >
+                #{keyword}
+              </Typography>
             ))}
-            </KeywordWrapper>
+          </KeywordWrapper>
         </CardContent>
-      <CardActions>
-        <Button size="small" color="primary" startIcon={<Share />}>
-          Share idea
-        </Button>
-      </CardActions>
-    </Card>
+        <CardActions>
+          <Button size="small" color="primary" startIcon={<Share />}>
+            Share idea
+          </Button>
+        </CardActions>
+      </Card>
     </Fade>
-  );
+  )
 }
